8172c9715d6454e9280aa479fc912e53f2e0608d
wanghansong <614158750@qq.com>
2018-05-13 10:19:31 +0800

    快排测试通过

--- 排序算法/挖坑分治法快排.js
+++ 排序算法/挖坑分治法快排.js
@@ -2,7 +2,7 @@
     1.i=L;j=R;基准数选择X=arr[i]挖一个坑
     2.j-- 从后往前找到比X小的数填到坑arr[i]中，此时多出了一个坑arr[j]
     3.i++ 从前往后找到比X大的一个数填到arr[j]坑中
-    4.重复2，3步骤知道i==j，将X基准数填入arr[i]中,返回此时基准数所在的位置index
+    4.重复2，3步骤直到i==j，将X基准数填入arr[i]中,返回此时基准数所在的位置index
 */
 function ajustArray(arr,l,r){
     var X=arr[l];
@@ -16,7 +16,7 @@ function ajustArray(arr,l,r){
             arr[i]=arr[j];
             i++;
         }
-        
+
         //从前往后找比X大的数，填到坑j处
         while(i<j && arr[i]<X){
             i--;
@@ -26,16 +26,21 @@ function ajustArray(arr,l,r){
             j--;
         }
     }
-    
+
     //此时i==j，将X基准数填入arr[i]中，返回此时基准数所在的位置index
     arr[i]=X;
     return i;
 }
-    
+
 function quickSort(arr,l,r){
     if(l<r){
         var index=ajustArray(arr,l,r);//一次快排
         quickSort(arr,l,index-1);//左边递归
         quickSort(arr,index+1,r);//右边递归
     }
+
+    return arr;
 }
+
+var arrTest=[3,23,4,1,6,1,7];
+console.log(quickSort(arrTest,0,6));
